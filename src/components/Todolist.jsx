54301cde00545b170bb3673495a119a09693b0ee
naim <swenaim35549@gmail.como>
2022-02-11 19:36:12 +0600

    first commit

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,48 +1,89 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState } from 'react';
 function Todolist() {
-
+    //  use states for the application 
     const [todoTitle, setTodotitle] = useState("");
     const [todoList, setTodolist] = useState([]);
+    const [editable,setEditable] = useState(false);
+    const [editableTodo,seteditableTodo]=useState(null);
 
-    const changeTodotitle = (val) => {
-        setTodotitle(val)
+    
+    // add a todo list 
+    const addTodolist = (event,title) => {
+        event.preventDefault();
+        if (title) {
+            const newTodo = {
+                id: Date.now(),
+                title
+            }
+            setTodolist([newTodo, ...todoList]);
+            setTodotitle("");
+        } else {
+            alert("Please Enter a valid title ..")
+        }
     }
 
-    const addTodolist = ((title) => {
-        const newTodo = {
-            id: Date.now(),
-            title: title,
+    // if any name have to delete the run the function
+    const deleteHandler = (todoid) => {
+        
+        const updateTodo = todoList.filter((item)=> item.id !== todoid);
+        setTodolist(updateTodo);
+    }
+
+    // is any todo name needs to be edit then run the function
+    const editHandler = (todoid)=>{
+        setEditable(true);
+        const updatetodoName=todoList.find((item) => item.id === todoid);
+        seteditableTodo(updatetodoName);
+        setTodotitle(updatetodoName.title);
+    }
+    // edit update handler
+    const editUpdateHandler = (event,title)=>{
+        event.preventDefault();
+        if (!title){
+            alert("Please edit and update your todo titler")
         }
-        setTodolist([newTodo, ...todoList]);
-
-    })
-
+        else{
+            
+            editableTodo.title = title || editableTodo.title;
+            setTodotitle("");
+            setEditable(false);
+            seteditableTodo(null);
+        }
+    }
+     
     return (
         <div className="main">
             <div className="todolist">
                 <form action="">
-                    <input type="text" name="addname" value={todoTitle} placeholder='Enter your todo' onChange={(e) => changeTodotitle(e.target.value)} />
-                    <button className="addbtn" onClick={(e) => {
-                        e.preventDefault();
-                        addTodolist(todoTitle);
-                        setTodotitle("");
+                    <input type="text" name="addname" value={todoTitle} placeholder='Enter your todo' onChange={(e) => setTodotitle(e.target.value)} />
+                    <button onClick = {(e) =>editable?editUpdateHandler (e, todoTitle):  addTodolist(e, todoTitle)} >
+                   {editable ? "Update Todo" : "Add Todo"}
+                   
+               </button>
 
-                    }}>Add Todo</button>
+               {/* <button onClick={(e) => addTodolist(e, todoTitle)}>Add Student</button>  */}
                 </form>
                 <br />
                 <form className="displaytodo" action="">
-                    {todoList.map((item) => (
+                    {todoList.map((todo) => (
                         <div className="list">
                             <div className="listdata">
-                                <div className="titlename">{item.title}</div> <br />
-                                <button className="editbtn">Edit</button>
-                                <button className="deletebtn" onClick={() => {
-                                    const deltodo = todoList.filter((todo) =>todo.id !==item.id);
-                                    setTodolist(deltodo);
-                                  
-                                    
-                                }}>Delete</button>
+                                <div className="titlename">{todo.title}</div> <br />
+                                <button className="editbtn" onClick={(e)=>{e.preventDefault();
+                                editHandler(todo.id);
+                                }}>Edit</button>
+                                <button className="deletebtn" /*onClick={(e) => {
+                                   e.preventDefault();
+                                   const newTodoList = todoList.filter((item) => item.id !== todo.id);
+                                   setTodolist(newTodoList);               
+                                }}> */
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        deleteHandler(todo.id);
+                                    }}
+                                >
+                                    Delete</button>
                             </div><hr />
 
                         </div>
